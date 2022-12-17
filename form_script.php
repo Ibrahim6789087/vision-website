c53fd834ce261e79842825b97098b88244f5bc8a
Ibrahim6789087 <120792027+Ibrahim6789087@users.noreply.github.com>
2022-12-17 14:09:17 +0000

    Add files via upload

---
+++ form_script.php
@@ -0,0 +1,28 @@
+
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+<title>ThankYou Page<title/>
+<head/>
+<body>
+<h1><a href="form.html">GO BACK<a/> <h1/>
+  (?php
+
+  $email=$_POST['email'];
+  $fname=$_POST['fname'];
+  $adr=$_POST['adress']
+  $data=$fname.",".$email.","$adress>
+
+  </address>;
+
+
+  $file="file.csv"
+
+
+  file_put_contents($file, $data . PHP_EOL, FILE_APPEND);
+  print "[h1 align=center]Thank you for submitting your email address![/h1]";
+
+
+  ?)
+ <body/>
+</html>
